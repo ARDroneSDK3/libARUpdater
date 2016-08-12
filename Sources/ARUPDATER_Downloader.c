c97e7dd75532adfd971a0de65eae59e85137ec7b
d.flattin <david.flattin@parrot.com>
2016-08-12 11:16:18 +0200

    [DEV] update blacklist bebop2
    Change-Id: Ifddfe095d1801676a98b72d983717cab0b89f4be
    Reviewed-on: https://gerrit.parrot.biz/47365
    Reviewed-by: Nicolas Brulez <nicolas.brulez@parrot.com>

--- Sources/ARUPDATER_Downloader.c
+++ Sources/ARUPDATER_Downloader.c
@@ -228,6 +228,9 @@ eARUPDATER_ERROR ARUPDATER_Downloader_New(ARUPDATER_Manager_t* manager, const ch
             downloader->blacklistedVersions[ARDISCOVERY_PRODUCT_EVINRUDE]->versions[0] = strdup("1.0.0");
             downloader->blacklistedVersions[ARDISCOVERY_PRODUCT_EVINRUDE]->nbVersionBlacklisted = 1;
 
+            downloader->blacklistedVersions[ARDISCOVERY_PRODUCT_BEBOP_2]->versions[0] = strdup("3.4.0");
+            downloader->blacklistedVersions[ARDISCOVERY_PRODUCT_BEBOP_2]->nbVersionBlacklisted = 1;
+
             downloader->blacklistedVersions[ARDISCOVERY_PRODUCT_MINIDRONE_DELOS3]->versions[0] = strdup("0.3.3");
             downloader->blacklistedVersions[ARDISCOVERY_PRODUCT_MINIDRONE_DELOS3]->nbVersionBlacklisted = 1;
 
